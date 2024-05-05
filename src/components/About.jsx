ea4f969f62551629835d53771a910167f1117913
Phattharaphon Khammun <k.pattara@outlook.com>
2024-05-05 22:19:48 +0700

    Updates

--- src/components/About.jsx
+++
@@ -1,6 +0,0 @@
-export default function About(){
-    return (
-    <>
-        <h1 className="text-blue-400">Hello from About</h1>
-    </>);
-}
--- src/components/Contact.jsx
+++
@@ -1,8 +0,0 @@
-export default function Contact(){
-    return (
-    <>
-        <h1>Hello from contact</h1>
-        <p>My name is Panchawat Buranathamrong</p>
-        <p>I currently live in Thailand</p>
-    </>);
-}
