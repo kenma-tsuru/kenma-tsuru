a50d49d844393f16f68ec508cf6c2efb404a31f8
Phattharaphon Khammun <k.pattara@outlook.com>
2024-05-05 22:09:15 +0700

    Add living address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,5 +3,6 @@ export default function Contact(){
     <>
         <h1>Hello from contact</h1>
         <p>My name is Panchawat Buranathamrong</p>
+        <p>I currently live in Thailand</p>
     </>);
 }
